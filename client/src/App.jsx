726607650d12b7aff264fbf1a7b2d432d8a9e523
oyolah <oyolah@outlook.com>
2024-06-22 00:49:10 +0300

    Add pages and routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Signin from "./pages/Signin/Signin";
+import Signup from "./pages/Signup/Signup";
+import Project from "./pages/Project/Project";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 const App = () => {
-    return <div className="text-red-500">App</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/sign-in" element={<Signin />} />
+                <Route path="/sign-up" element={<Signup />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/project" element={<Project />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
