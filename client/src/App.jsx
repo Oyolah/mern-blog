923813cc1813e4f5874cbdbf366142bc9820c2c9
oyolah <oyolah@outlook.com>
2024-06-22 01:58:04 +0300

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,21 @@ import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Signin from "./pages/Signin/Signin";
 import Signup from "./pages/Signup/Signup";
-import Project from "./pages/Project/Project";
+import Projects from "./pages/Projects/Projects";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Header from "./components/Header/Header";
 
 const App = () => {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/sign-in" element={<Signin />} />
                 <Route path="/sign-up" element={<Signup />} />
                 <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/project" element={<Project />} />
+                <Route path="/projects" element={<Projects />} />
             </Routes>
         </BrowserRouter>
     );
